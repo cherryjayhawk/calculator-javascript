6b8084ff47c65d8a62cdfa6c1fe4c807f681a57e
cherryjayhawk <muhbintangr19@gmail.com>
2023-03-08 13:04:14 +0700

    membuat calculator menggunakan javascrip, html, dan css

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function calculator(value) {
+    document.getElementById('result').value += value;
+  }
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  function calculateResult() {
+    var result = document.getElementById('result').value;
+    var answer = eval(result);
+    document.getElementById('result').value = answer;
+  }
+  
